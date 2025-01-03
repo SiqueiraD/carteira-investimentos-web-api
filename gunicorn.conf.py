5b25a0a36796c422d065ab9f5e452f1eee1a9b05
Danilosic <danilosic.oliveira@gmail.com>
2025-01-03 00:04:31 -0300

    ajuste na publicacao para executar

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,21 +1,22 @@
 import multiprocessing
 
-# Configurações do servidor
+# Server socket
 bind = "0.0.0.0:8000"
+backlog = 2048
+
+# Worker processes
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 600
 
-# Configurações de log
+# Logging
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
 
-# Configurações de reload e debug
-reload = False
-reload_engine = "auto"
+# Process naming
+proc_name = "investimentos-api"
 
-# Configurações de segurança
-limit_request_line = 4094
-limit_request_fields = 100
-limit_request_field_size = 8190
+# SSL config
+keyfile = None
+certfile = None
